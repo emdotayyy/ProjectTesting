1d0f9ea106777b7a45e8f0d0620d1729349c2b3d
Aditya Gupta <aditya.gupta4@globallogic.com>
2022-08-01 10:15:47 +0530

    Aditya Changes

--- ProjectTesting/Program.cs
+++ ProjectTesting/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProjectTesting.Data;
 using ProjectTesting.Models;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.Configure<StripeSetting>(builder.Configuration.GetSection("Stripe"));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
@@ -21,7 +23,7 @@ builder.Services.AddSession();
 var app = builder.Build();
 
 
-Stripe.StripeConfiguration.SetApiKey("pk_test_51LRA7qSB35RrtZsgugNGSkiNIVwvVizKByJALcSh3BbKMj4aoaDnMYN08uZEpogvNZxmKzfO0wIt9ln1WywmhjNS00WBC1e9Dx");
+//Stripe.StripeConfiguration.SetApiKey("pk_test_51LRA7qSB35RrtZsgugNGSkiNIVwvVizKByJALcSh3BbKMj4aoaDnMYN08uZEpogvNZxmKzfO0wIt9ln1WywmhjNS00WBC1e9Dx");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -39,6 +41,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
 app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
